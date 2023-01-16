6bea296ebc694d13763a7af02adf498e20fb044d
nikki dekerf <nikkidekerf@gmail.com>
2023-01-16 21:02:31 +0100

    Works and design is better but needs refactoring between view and controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,19 @@
 class GamesController < ApplicationController
 
   def score
+    @word = params[:word]
+    require 'open-uri'
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    @dictionary = JSON.parse(URI.open(url).read)
     @randomletters = params[:randomletters]
     @randomwordletters = @randomletters.split(' ')
-    @word = params[:word]
     @userwordletters = @word.split('')
     # The word can’t be built out of the original grid
-
-    # The word is valid according to the grid, but is not a valid English word
-    # The word is valid according to the grid and is an English word
+    @build = @userwordletters.all? { |letter| @randomwordletters.include?(letter) }
   end
 
   def new
     # create a new @letters instance variable storing these random letters from the alphabet.
     @letters = ('a'..'z').to_a.sample(10)
   end
-
-  # private
-  # methods go under this line ---------
-
 end
